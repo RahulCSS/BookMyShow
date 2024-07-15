06bf0d28f196b5aa3cd42761402da193a5e8ec59
Rahulcss <Rahulcs1618@gmail.com>
2024-07-15 23:17:31 +0530

    Login & Registration request

--- bmsserver/routes/userRoutes.js
+++ bmsserver/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
 
 router.post('/register',async (request, response) => {
     try {
         const userExists = await User.findOne({email: request.body.email });
-
+        
         if(userExists) {
             response.status(403).send({
                 success: false,
@@ -12,6 +13,10 @@ router.post('/register',async (request, response) => {
             });
             return;
         }
+        //* Hashing
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(request.body.password, salt);
+        request.body.password = hashedPassword;
 
         const newUser = new User(request.body);
         await newUser.save();
@@ -28,5 +33,33 @@ router.post('/register',async (request, response) => {
         });
     };
 });
+
+router.post('/login',async (request, response) => {
+    try{
+        const user = await User.findOne({email: request.body.email });
+        const validPassword = await bcrypt.compare(request.body.password, user.password);
+        
+        if(!user || !validPassword) {
+            response.status(403).send({
+                success: false,
+                message: 'Invalid Credentials'
+            });
+            return;
+        }
+
+        response.status(200).send({ 
+            success: true,
+            message: 'User logged in successfully',
+        });
+        
+    }catch{
+        console.error(err);
+        response.status(403).json({ 
+            success: false,
+            message: 'Failed to register user,Please try again later'
+        });
+    }
+});
+
 
 module.exports = router;
